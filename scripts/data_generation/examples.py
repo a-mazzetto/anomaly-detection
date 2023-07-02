2d322d132ff4f8961a5de1eeb2a78c034d347101
a-mazzetto <92179628+a-mazzetto@users.noreply.github.com>
2023-07-02 17:14:31 +0200

    poisson process data generation

--- scripts/data_generation/examples.py
+++ scripts/data_generation/examples.py
@@ -1,11 +1,11 @@
 """Test data generation"""
 # %% Imports
 import time
+import numpy as np
 import matplotlib.pyplot as plt
 
-from data_generation.data_generation import generate_pitman_yor
-
 # %% Check Pitman-Yor data generation
+from data_generation.data_generation import generate_pitman_yor
 intensity = 7.0
 discount = 0.25
 
@@ -22,5 +22,39 @@ print(f"Simulated in {dt}")
 plt.hist(sequence)
 plt.xticks(rotation = 75)
 plt.title(f'Pitman-Yor samples distribution with intensity {intensity} discount {discount}')
+
+# %% Check Poisson process data generation
+from data_generation.data_generation import generate_poisson_process
+
+intensity = 1.
+t0 = time.time()
+time_sequence_0 = generate_poisson_process(
+    intensity=intensity,
+    length=100,
+    seed=0)
+dt = time.time() - t0
+print(f"Simulated in {dt}")
+
+t0 = time.time()
+time_sequence_1 = generate_poisson_process(
+    intensity=intensity,
+    max=118.1,
+    seed=0)
+dt = time.time() - t0
+print(f"Simulated in {dt}")
+
+try:
+    time_sequence = generate_poisson_process(
+        intensity=intensity,
+        length=100,
+        max=118.1,
+        seed=0)
+except ValueError as e:
+    print(e)
+
+fig, ax = plt.subplots(1, 2)
+ax[0].plot(time_sequence_0)
+ax[0].plot(time_sequence_1)
+ax[1].hist(np.diff(time_sequence_0))
 
 # %%
