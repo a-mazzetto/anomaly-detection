65d52408f600960940559d552b4419a48ac97760
a-mazzetto <92179628+a-mazzetto@users.noreply.github.com>
2023-07-02 16:40:06 +0200

    create library

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='anomaly-detection',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Final project Library',
+    author='Alberto Mazzetto',
+    license='MIT',
+)
