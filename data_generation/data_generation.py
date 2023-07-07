b88154a7852df05212392ec128187f3409238c84
a-mazzetto <92179628+a-mazzetto@users.noreply.github.com>
2023-07-07 19:44:46 +0200

    data generation func. into blocks

--- data_generation/data_generation.py
+++ data_generation/data_generation.py
@@ -16,7 +16,7 @@ def generate_dataset(
     source_discounts: Union[float, Callable],
     node_names: Optional[List[str]]=None,
     seed: Optional[int]=None,
-    dicretize_time: bool=False,
+    discretize_time: bool=False,
     file_name: Optional[str]=None):
     """Function to generate the dataset.
 
@@ -38,6 +38,22 @@ def generate_dataset(
     :param file_name: save to file if filename present
     """
     gen = np.random.default_rng(seed=seed)
+    # generate time sequence
+    times = generate_time_sequence(period=period, max_time=max_time, n_nodes=n_nodes, gen=gen)
+    # Destinations PY/DP process
+    destinations = generate_destination_sequence(discount=destination_discount,
+        intensity=destination_intensity, node_names=node_names, times=times, gen=gen)
+    # Finally, generate source conditioned upon destinations
+    sources, source_intensity_vector, source_discount_vector = generate_source_sequence(
+        discounts=source_discounts, intensities=source_intensities,
+        node_names=node_names, destinations=destinations, gen=gen)
+    # Create dataset
+    dataset = create_and_save_dataset(times=times, destinations=destinations, sources=sources,
+                                      discretize_time=discretize_time, file_name=file_name)
+    return dataset, source_intensity_vector, source_discount_vector
+
+def generate_time_sequence(period, max_time, n_nodes, gen):
+    """Helper function to generate time sequence"""
     # Inhomogeneous Poisson Process with rates to emulate LANL dataset
     # From LANL dataset: 0.8-1.4M/4h connections with 12000 nodes
     lambda_min_max = np.array((0.8, 1.4)) * 1e6 / 12000 / (4 * 60 * 60) * n_nodes
@@ -46,33 +62,45 @@ def generate_dataset(
     times = inhomogeneous_poisson_process_sinusoidal(
         lambda_0, lambda_1, period, max_time, gen=gen
         )
-    # Destinations PY/DP process
+    return times
+
+def generate_destination_sequence(discount, intensity, node_names, times, gen):
+    """Helper function to generate destination sequency"""
     destinations = generate_pitman_yor(
-        discount=destination_discount,
-        intensity=destination_intensity,
+        discount=discount,
+        intensity=intensity,
         length=len(times),
         labels=node_names,
         seed=gen
     )
-    # Finally, generate source conditioned upon destinations
-    unique_destinations = np.unique(destinations)
+    return destinations
+
+def _parse_source_pitman_yor_parameters(discounts, intensities, unique_destinations, gen):
+    """Helper function to parse source parameters"""
     n_unique_destinations = len(unique_destinations)
-    if isinstance(source_intensities, (float, int)):
-        source_intensity_vector = source_intensities * np.ones(len(destinations))
+    if isinstance(intensities, (float, int)):
+        source_intensity_vector = intensities * np.ones(n_unique_destinations)
     else:
-        source_intensity_vector = source_intensities(
-            gen=gen,
-            size=n_unique_destinations)
-    if isinstance(source_discounts, (float, int)):
-        source_discount_vector = source_discounts * np.ones(len(destinations))
+        source_intensity_vector = intensities(gen=gen, size=n_unique_destinations)
+    if isinstance(discounts, (float, int)):
+        source_discount_vector = discounts * np.ones(n_unique_destinations)
     else:
-        source_discount_vector = source_discounts(
-            gen=gen,
-            size=n_unique_destinations)
+        source_discount_vector = discounts(gen=gen, size=n_unique_destinations)
+    return source_intensity_vector, source_discount_vector
+
+def generate_source_sequence(discounts, intensities, node_names, destinations, gen):
+    """Helper function to calculate sources conditional upon destinations.
+    Recall that `discounts` and `intensities` can be Callable"""
+    unique_destinations = np.unique(destinations)
+    source_intensity_vector, source_discount_vector = _parse_source_pitman_yor_parameters(
+        discounts=discounts,
+        intensities=intensities,
+        unique_destinations=unique_destinations,
+        gen=gen
+    )
+    # Generate processes
     link_dict = {}
-    for _dest, _int, _disc in zip(unique_destinations,
-                               source_intensity_vector,
-                               source_discount_vector):
+    for _dest, _int, _disc in zip(unique_destinations, source_intensity_vector, source_discount_vector):
         _n = destinations.count(_dest)
         _sources = generate_pitman_yor(
             discount=_disc,
@@ -82,16 +110,19 @@ def generate_dataset(
             seed=gen
         )
         link_dict[_dest] = _sources
-    # Create link list preserving generation order
-    dataset = []
-    for _time, _dest in zip(times, destinations):
-        dataset.append((
-            _time if not dicretize_time else np.floor(_time).astype(int),
-            link_dict[_dest].pop(0),
-            _dest))
+    # Return list of sources preserving chronological order
+    sequence = [link_dict[_dest].pop(0) for _dest in destinations]
+    assert all(len(i) == 0 for i in link_dict.values()), 'All sources should be consumed by now!'
+    return sequence, source_intensity_vector, source_discount_vector
+
+def create_and_save_dataset(times, destinations, sources, discretize_time, file_name):
+    """Helper function to create and save dataset"""
+    if discretize_time:
+        times = np.floor(times).astype(int)
+    dataset = list(zip(times, sources, destinations))
     # Save dataset if required
     if file_name is not None:
         with open(file_name, 'w', encoding='utf-8') as file:
             for line in dataset:
-                file.write(' '.join(str(s) for s in line) + '\n')
-    return dataset, source_intensity_vector, source_discount_vector
+                file.write('\t'.join(str(s) for s in line) + '\n')
+    return dataset
