041d33abcb3244d0ef95117f1b1e09f407e6e76c
a-mazzetto <92179628+a-mazzetto@users.noreply.github.com>
2023-07-09 12:01:42 +0200

    dirichlet process parameter estimation

--- parameter_estimation/parameter_estimation.py
+++ parameter_estimation/parameter_estimation.py
@@ -28,3 +28,23 @@ def pitman_yor_est_pars(meas_kn, meas_h1n, n, n_nodes):
         return d_hat * gamma(d_hat + x) * kn_hat - gamma(1 + x) * n**d_hat
     alpha_hat = fsolve(_equation, kn_hat / np.log(n))[0]
     return alpha_hat, d_hat
+
+def dirichlet_true_kn(true_alpha, n, n_nodes):
+    """Parameter estimation of Pitman-Yor parameters following the dissertation presented in
+    ``Modelling Dynamic Network Evolution as Pitman-Yor Process" by Passino and Heard
+    """
+    true_kn = true_alpha * np.log(n)
+    # Correct for discrete base measures
+    true_kn_corr = n_nodes * (1 - ((n_nodes - 1) / n_nodes)**true_kn)
+    return true_kn_corr
+
+def dirichlet_est_pars(meas_kn, n, n_nodes):
+    """Parameter estimation of Pitman-Yor parameters following the dissertation presented in
+    ``Modelling Dynamic Network Evolution as Pitman-Yor Process" by Passino and Heard. In
+    particular, we use here only the best estimation technique.
+    """
+    # Adjustment under the Birthday Problem
+    kn_hat = np.log(1 - meas_kn / n_nodes) / np.log((n_nodes - 1) / n_nodes)
+    # Estimate parameters under approximation `Method 2` proposed in the paper
+    alpha_hat = kn_hat / np.log(n)
+    return alpha_hat
