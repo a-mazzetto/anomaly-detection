d9eb595191cb5e1657105f70ce5046beddfb3de2
a-mazzetto <92179628+a-mazzetto@users.noreply.github.com>
2023-07-09 12:46:02 +0200

    parameter estimation example

---
+++ scripts/parameters_estimation/ddcrp_example.py
@@ -0,0 +1,53 @@
+"""Parameter estimation with DDCRP"""
+# %%
+from collections import Counter
+import numpy as np
+import matplotlib.pyplot as plt
+from processes.dirichlet import generate_exp_ddcrp
+from processes.poisson import poisson_process
+from parameter_estimation.parameter_estimation import \
+    dirichlet_true_kn, dirichlet_est_pars
+
+# %% Initial parameters
+
+N_NODES = 100
+N_ITERS = 1000
+N = 1000
+TRUE_ALPHA = 5.0
+
+true_kn = dirichlet_true_kn(TRUE_ALPHA, N, N_NODES)
+
+# %% Simulate processes and estimate Kn and H1n
+meas_kn_list = []
+meas_alpha_list = []
+for _ in range(N_ITERS):
+    times = poisson_process(
+        rate=10.0,
+        length=N
+    )
+    sequence = generate_exp_ddcrp(
+        intensity=TRUE_ALPHA,
+        decay=1.0,
+        times=times,
+        labels=N_NODES
+    )
+    counter = Counter(sequence)
+    meas_kn = len(counter)
+    meas_alpha = dirichlet_est_pars(meas_kn, N, N_NODES)
+    meas_kn_list.append(meas_kn)
+    meas_alpha_list.append(meas_alpha)
+
+# %%
+fig, ax = plt.subplots(1, 2)
+fig.tight_layout()
+ax[0].hist(meas_alpha_list, alpha=0.5)
+ax[0].vlines(x=TRUE_ALPHA, ymin=0, ymax=N_ITERS/4, linestyle="--", color="red")
+ax[0].set_xlabel(r"$\alpha$")
+ax[0].set_ylabel("Density")
+
+ax[1].hist(meas_kn_list, alpha=0.5)
+ax[1].vlines(x=true_kn, ymin=0, ymax=N_ITERS/4, linestyle="--", color="red")
+ax[1].set_xlabel(r"$K_n$")
+ax[1].set_ylabel("Density")
+
+# %%
---
+++ scripts/parameters_estimation/pitman_yor_example.py
@@ -0,0 +1,62 @@
+"""Parameter estimation for Pitman-Yor process"""
+# %%
+from collections import Counter
+import numpy as np
+import matplotlib.pyplot as plt
+from processes.pitman_yor import generate_pitman_yor
+from parameter_estimation.parameter_estimation import \
+    pitman_yor_true_kn_h1n, pitman_yor_est_pars
+
+# %% Initial parameters
+
+N_NODES = 100
+N_ITERS = 1000
+N = 1000
+TRUE_ALPHA = 5.0
+TRUE_D = 0.25
+
+true_kn, true_h1n = pitman_yor_true_kn_h1n(TRUE_ALPHA, TRUE_D, N, N_NODES)
+
+# %% Simulate processes and estimate Kn and H1n
+meas_kn_list = []
+meas_h1n_list = []
+meas_alpha_list = []
+meas_d_list = []
+for _ in range(N_ITERS):
+    sequence = generate_pitman_yor(
+        discount=TRUE_D,
+        intensity=TRUE_ALPHA,
+        length=N,
+        labels=N_NODES
+    )
+    counter = Counter(sequence)
+    meas_kn = len(counter)
+    meas_h1n = sum(np.equal(list(counter.values()), 1))
+    meas_alpha, meas_d = pitman_yor_est_pars(meas_kn, meas_h1n, N, N_NODES)
+    meas_kn_list.append(meas_kn)
+    meas_h1n_list.append(meas_h1n)
+    meas_alpha_list.append(meas_alpha)
+    meas_d_list.append(meas_d)
+
+# %%
+fig, ax = plt.subplots(2, 2)
+fig.tight_layout()
+ax[0, 0].hist(meas_alpha_list, alpha=0.5)
+ax[0, 0].vlines(x=TRUE_ALPHA, ymin=0, ymax=N_ITERS/4, linestyle="--", color="red")
+ax[0, 0].set_xlabel(r"$\alpha$")
+ax[0, 0].set_ylabel("Density")
+
+ax[0, 1].hist(meas_d_list, alpha=0.5)
+ax[0, 1].vlines(x=TRUE_D, ymin=0, ymax=N_ITERS/4, linestyle="--", color="red")
+ax[0, 1].set_xlabel(r"$d$")
+
+ax[1, 0].hist(meas_kn_list, alpha=0.5)
+ax[1, 0].vlines(x=true_kn, ymin=0, ymax=N_ITERS/4, linestyle="--", color="red")
+ax[1, 0].set_xlabel(r"$K_n$")
+ax[1, 0].set_ylabel("Density")
+
+ax[1, 1].hist(meas_h1n_list, alpha=0.5)
+ax[1, 1].vlines(x=true_h1n, ymin=0, ymax=N_ITERS/4, linestyle="--", color="red")
+ax[1, 1].set_xlabel(r"$H_{1n}$")
+
+# %%
