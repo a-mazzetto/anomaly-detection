6d14d18ada79420b2dab604ac0c04c36a923d0b8
a-mazzetto <92179628+a-mazzetto@users.noreply.github.com>
2023-07-21 18:47:24 +0200

    VGRNN paper replication

---
+++ scripts/vgrnn/paper_replication.py
@@ -0,0 +1,379 @@
+"""Testing of building blocks"""
+# %% Imports
+import time
+import pickle
+import numpy as np
+import torch
+from torch.autograd import Variable
+
+# %% Load Facebook dataset
+with open('./data/Facebook/adj_orig_dense_list.pickle', 'rb') as handle:
+    adj_orig_dense_list = pickle.load(handle, encoding='bytes')
+
+with open('./data/Facebook/adj_time_list.pickle', 'rb') as handle:
+    adj_time_list = pickle.load(handle, encoding='latin1')
+
+# %% Parametrization
+h_dim = 32
+z_dim = 16
+n_layers =  1
+clip = 10
+learning_rate = 1e-2
+seq_len = len(adj_orig_dense_list)
+num_nodes = adj_orig_dense_list[seq_len-1].shape[0]
+x_dim = num_nodes
+eps = 1e-10
+conv_type='GCN'
+
+# %% Features
+x_in_list = []
+for i in range(0, seq_len):
+    x_temp = torch.tensor(np.eye(num_nodes).astype(np.float32))
+    x_in_list.append(torch.tensor(x_temp))
+
+x_in = Variable(torch.stack(x_in_list))
+print(x_in.shape)
+
+# %% Edge index
+edge_idx_list = []
+for adj in adj_orig_dense_list:
+    edge_idx_list.append(
+        torch.tensor(np.vstack(np.where(adj_orig_dense_list[0]))))
+
+# %% Model
+from vgrnn.vgrnn import VGRNN
+
+vgrnn_model = VGRNN(x_dim, h_dim, z_dim, n_layers, eps, conv=conv_type, bias=True)
+
+vgrnn_model(x_in, edge_idx_list, adj_orig_dense_list)
+
+# %% Utilities for paper case
+import scipy.sparse as sp
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import average_precision_score
+
+def get_roc_scores(edges_pos, edges_neg, adj_orig_dense_list, embs):
+    def sigmoid(x):
+        return 1 / (1 + np.exp(-x))
+    
+    auc_scores = []
+    ap_scores = []
+    
+    for i in range(len(edges_pos)):
+        # Predict on test set of edges
+        emb = embs[i].detach().numpy()
+        adj_rec = np.dot(emb, emb.T)
+        adj_orig_t = adj_orig_dense_list[i]
+        preds = []
+        pos = []
+        for e in edges_pos[i]:
+            preds.append(sigmoid(adj_rec[e[0], e[1]]))
+            pos.append(adj_orig_t[e[0], e[1]])
+            
+        preds_neg = []
+        neg = []
+        for e in edges_neg[i]:
+            preds_neg.append(sigmoid(adj_rec[e[0], e[1]]))
+            neg.append(adj_orig_t[e[0], e[1]])
+        
+        preds_all = np.hstack([preds, preds_neg])
+        labels_all = np.hstack([np.ones(len(preds)), np.zeros(len(preds_neg))])
+        auc_scores.append(roc_auc_score(labels_all, preds_all))
+        ap_scores.append(average_precision_score(labels_all, preds_all))
+
+    return auc_scores, ap_scores
+
+def sparse_to_tuple(sparse_mx):
+    if not sp.isspmatrix_coo(sparse_mx):
+        sparse_mx = sparse_mx.tocoo()
+    coords = np.vstack((sparse_mx.row, sparse_mx.col)).transpose()
+    values = sparse_mx.data
+    shape = sparse_mx.shape
+    return coords, values, shape
+
+def mask_edges_det(adjs_list):
+    adj_train_l, train_edges_l, val_edges_l = [], [], []
+    val_edges_false_l, test_edges_l, test_edges_false_l = [], [], []
+    edges_list = []
+    for i in range(0, len(adjs_list)):
+        # Function to build test set with 10% positive links
+        # NOTE: Splits are randomized and results might slightly deviate from reported numbers in the paper.
+        
+        adj = adjs_list[i]
+        # Remove diagonal elements
+        adj = adj - sp.dia_matrix((adj.diagonal()[np.newaxis, :], [0]), shape=adj.shape)
+        adj.eliminate_zeros()
+        # Check that diag is zero:
+        assert np.diag(adj.todense()).sum() == 0
+        
+        adj_triu = sp.triu(adj)
+        adj_tuple = sparse_to_tuple(adj_triu)
+        edges = adj_tuple[0]
+        edges_all = sparse_to_tuple(adj)[0]
+        num_test = int(np.floor(edges.shape[0] / 10.))
+        num_val = int(np.floor(edges.shape[0] / 20.))
+        
+        all_edge_idx = list(range(edges.shape[0]))
+        np.random.shuffle(all_edge_idx)
+        val_edge_idx = all_edge_idx[:num_val]
+        test_edge_idx = all_edge_idx[num_val:(num_val + num_test)]
+        test_edges = edges[test_edge_idx]
+        val_edges = edges[val_edge_idx]
+        train_edges = np.delete(edges, np.hstack([test_edge_idx, val_edge_idx]), axis=0)
+        
+        edges_list.append(edges)
+        
+        def ismember(a, b, tol=5):
+            rows_close = np.all(np.round(a - b[:, None], tol) == 0, axis=-1)
+            return np.any(rows_close)
+
+        test_edges_false = []
+        while len(test_edges_false) < len(test_edges):
+            idx_i = np.random.randint(0, adj.shape[0])
+            idx_j = np.random.randint(0, adj.shape[0])
+            if idx_i == idx_j:
+                continue
+            if ismember([idx_i, idx_j], edges_all):
+                continue
+            if test_edges_false:
+                if ismember([idx_j, idx_i], np.array(test_edges_false)):
+                    continue
+                if ismember([idx_i, idx_j], np.array(test_edges_false)):
+                    continue
+            test_edges_false.append([idx_i, idx_j])
+
+        val_edges_false = []
+        while len(val_edges_false) < len(val_edges):
+            idx_i = np.random.randint(0, adj.shape[0])
+            idx_j = np.random.randint(0, adj.shape[0])
+            if idx_i == idx_j:
+                continue
+            if ismember([idx_i, idx_j], train_edges):
+                continue
+            if ismember([idx_j, idx_i], train_edges):
+                continue
+            if ismember([idx_i, idx_j], val_edges):
+                continue
+            if ismember([idx_j, idx_i], val_edges):
+                continue
+            if val_edges_false:
+                if ismember([idx_j, idx_i], np.array(val_edges_false)):
+                    continue
+                if ismember([idx_i, idx_j], np.array(val_edges_false)):
+                    continue
+            val_edges_false.append([idx_i, idx_j])
+
+        assert ~ismember(test_edges_false, edges_all)
+        assert ~ismember(val_edges_false, edges_all)
+        assert ~ismember(val_edges, train_edges)
+        assert ~ismember(test_edges, train_edges)
+        assert ~ismember(val_edges, test_edges)
+
+        data = np.ones(train_edges.shape[0])
+
+        # Re-build adj matrix
+        adj_train = sp.csr_matrix((data, (train_edges[:, 0], train_edges[:, 1])), shape=adj.shape)
+        adj_train = adj_train + adj_train.T
+
+        adj_train_l.append(adj_train)
+        train_edges_l.append(train_edges)
+        val_edges_l.append(val_edges)
+        val_edges_false_l.append(val_edges_false)
+        test_edges_l.append(test_edges)
+        test_edges_false_l.append(test_edges_false)
+
+    # NOTE: these edge lists only contain single direction of edge!
+    return adj_train_l, train_edges_l, val_edges_l, val_edges_false_l, test_edges_l, test_edges_false_l
+
+def mask_edges_prd(adjs_list):
+    pos_edges_l , false_edges_l = [], []
+    edges_list = []
+    for i in range(0, len(adjs_list)):
+        # Function to build test set with 10% positive links
+        # NOTE: Splits are randomized and results might slightly deviate from reported numbers in the paper.
+        
+        adj = adjs_list[i]
+        # Remove diagonal elements
+        adj = adj - sp.dia_matrix((adj.diagonal()[np.newaxis, :], [0]), shape=adj.shape)
+        adj.eliminate_zeros()
+        # Check that diag is zero:
+        assert np.diag(adj.todense()).sum() == 0
+        
+        adj_triu = sp.triu(adj)
+        adj_tuple = sparse_to_tuple(adj_triu)
+        edges = adj_tuple[0]
+        edges_all = sparse_to_tuple(adj)[0]
+        num_false = int(edges.shape[0])
+        
+        pos_edges_l.append(edges)
+        
+        def ismember(a, b, tol=5):
+            rows_close = np.all(np.round(a - b[:, None], tol) == 0, axis=-1)
+            return np.any(rows_close)
+        
+        edges_false = []
+        while len(edges_false) < num_false:
+            idx_i = np.random.randint(0, adj.shape[0])
+            idx_j = np.random.randint(0, adj.shape[0])
+            if idx_i == idx_j:
+                continue
+            if ismember([idx_i, idx_j], edges_all):
+                continue
+            if edges_false:
+                if ismember([idx_j, idx_i], np.array(edges_false)):
+                    continue
+                if ismember([idx_i, idx_j], np.array(edges_false)):
+                    continue
+            edges_false.append([idx_i, idx_j])
+
+        assert ~ismember(edges_false, edges_all)
+        
+        false_edges_l.append(edges_false)
+
+    # NOTE: these edge lists only contain single direction of edge!
+    return pos_edges_l, false_edges_l
+
+def mask_edges_prd_new(adjs_list, adj_orig_dense_list):
+    pos_edges_l , false_edges_l = [], []
+    edges_list = []
+    
+    # Function to build test set with 10% positive links
+    # NOTE: Splits are randomized and results might slightly deviate from reported numbers in the paper.
+
+    adj = adjs_list[0]
+    # Remove diagonal elements
+    adj = adj - sp.dia_matrix((adj.diagonal()[np.newaxis, :], [0]), shape=adj.shape)
+    adj.eliminate_zeros()
+    # Check that diag is zero:
+    assert np.diag(adj.todense()).sum() == 0
+
+    adj_triu = sp.triu(adj)
+    adj_tuple = sparse_to_tuple(adj_triu)
+    edges = adj_tuple[0]
+    edges_all = sparse_to_tuple(adj)[0]
+    num_false = int(edges.shape[0])
+
+    pos_edges_l.append(edges)
+
+    def ismember(a, b, tol=5):
+        rows_close = np.all(np.round(a - b[:, None], tol) == 0, axis=-1)
+        return np.any(rows_close)
+
+    edges_false = []
+    while len(edges_false) < num_false:
+        idx_i = np.random.randint(0, adj.shape[0])
+        idx_j = np.random.randint(0, adj.shape[0])
+        if idx_i == idx_j:
+            continue
+        if ismember([idx_i, idx_j], edges_all):
+            continue
+        if edges_false:
+            if ismember([idx_j, idx_i], np.array(edges_false)):
+                continue
+            if ismember([idx_i, idx_j], np.array(edges_false)):
+                continue
+        edges_false.append([idx_i, idx_j])
+
+    assert ~ismember(edges_false, edges_all)    
+    false_edges_l.append(np.asarray(edges_false))
+    
+    for i in range(1, len(adjs_list)):
+        edges_pos = np.transpose(np.asarray(np.where((adj_orig_dense_list[i] - adj_orig_dense_list[i-1])>0)))
+        num_false = int(edges_pos.shape[0])
+        
+        adj = adjs_list[i]
+        # Remove diagonal elements
+        adj = adj - sp.dia_matrix((adj.diagonal()[np.newaxis, :], [0]), shape=adj.shape)
+        adj.eliminate_zeros()
+        # Check that diag is zero:
+        assert np.diag(adj.todense()).sum() == 0
+        
+        adj_triu = sp.triu(adj)
+        adj_tuple = sparse_to_tuple(adj_triu)
+        edges = adj_tuple[0]
+        edges_all = sparse_to_tuple(adj)[0]
+        
+        edges_false = []
+        while len(edges_false) < num_false:
+            idx_i = np.random.randint(0, adj.shape[0])
+            idx_j = np.random.randint(0, adj.shape[0])
+            if idx_i == idx_j:
+                continue
+            if ismember([idx_i, idx_j], edges_all):
+                continue
+            if edges_false:
+                if ismember([idx_j, idx_i], np.array(edges_false)):
+                    continue
+                if ismember([idx_i, idx_j], np.array(edges_false)):
+                    continue
+            edges_false.append([idx_i, idx_j])
+        
+        assert ~ismember(edges_false, edges_all)
+        
+        false_edges_l.append(np.asarray(edges_false))
+        pos_edges_l.append(edges_pos)
+    
+    # NOTE: these edge lists only contain single direction of edge!
+    return pos_edges_l, false_edges_l
+
+# %% Other operations paper-specific
+outs = mask_edges_det(adj_time_list)
+train_edges_l = outs[1]
+
+pos_edges_l, false_edges_l = mask_edges_prd(adj_time_list)
+
+pos_edges_l_n, false_edges_l_n = mask_edges_prd_new(adj_time_list, adj_orig_dense_list)
+
+# %%
+optimizer = torch.optim.Adam(vgrnn_model.parameters(), lr=learning_rate)
+
+seq_start = 0
+seq_end = seq_len - 3
+tst_after = 0
+
+for k in range(1000):
+    optimizer.zero_grad()
+    start_time = time.time()
+    kld_loss, nll_loss, _, _, hidden_st = vgrnn_model(x_in[seq_start:seq_end],
+                                                      edge_idx_list[seq_start:seq_end],
+                                                      adj_orig_dense_list[seq_start:seq_end])
+    loss = kld_loss + nll_loss
+    loss.backward()
+    optimizer.step()
+    
+    torch.nn.utils.clip_grad_norm_(vgrnn_model.parameters(), clip)
+    
+    if k>tst_after:
+        _, _, enc_means, pri_means, _ = vgrnn_model(x_in[seq_end:seq_len],
+                                                    edge_idx_list[seq_end:seq_len],
+                                                    adj_orig_dense_list[seq_end:seq_len],
+                                                    hidden_st)
+        
+        auc_scores_prd, ap_scores_prd = get_roc_scores(pos_edges_l[seq_end:seq_len],
+                                                       false_edges_l[seq_end:seq_len],
+                                                       adj_orig_dense_list[seq_end:seq_len],
+                                                       pri_means)
+        
+        auc_scores_prd_new, ap_scores_prd_new = get_roc_scores(pos_edges_l_n[seq_end:seq_len],
+                                                               false_edges_l_n[seq_end:seq_len],
+                                                               adj_orig_dense_list[seq_end:seq_len],
+                                                               pri_means)
+        
+    
+    print('epoch: ', k)
+    print('kld_loss =', kld_loss.mean().item())
+    print('nll_loss =', nll_loss.mean().item())
+    print('loss =', loss.mean().item())
+    if k>tst_after:
+        print('----------------------------------')
+        print('Link Prediction')
+        print('link_prd_auc_mean', np.mean(np.array(auc_scores_prd)))
+        print('link_prd_ap_mean', np.mean(np.array(ap_scores_prd)))
+        print('----------------------------------')
+        print('New Link Prediction')
+        print('new_link_prd_auc_mean', np.mean(np.array(auc_scores_prd_new)))
+        print('new_link_prd_ap_mean', np.mean(np.array(ap_scores_prd_new)))
+        print('----------------------------------')
+    print('----------------------------------')
+
+# %%
---
+++ vgrnn/vgrnn.py
@@ -0,0 +1,183 @@
+"""VGRNN imlementation following https://arxiv.org/abs/1908.09710"""
+import torch
+from torch.nn import Sequential, Linear, ReLU, Softplus
+from torch_geometric.nn import Sequential as GeomSequential
+from torch_geometric.nn.conv import GCNConv, SAGEConv, GINConv
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+class Graph_GRU_SAGE(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, n_layer, bias=True):
+        super(Graph_GRU_SAGE, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.n_layer = n_layer
+        
+        # GRU weights
+        self.weight_xz = []
+        self.weight_hz = []
+        self.weight_xr = []
+        self.weight_hr = []
+        self.weight_xh = []
+        self.weight_hh = []
+        
+        for i in range(self.n_layer):
+            _input_size = input_size if i == 0 else hidden_size
+            self.weight_xz.append(SAGEConv(_input_size, hidden_size, bias=bias))
+            self.weight_hz.append(SAGEConv(hidden_size, hidden_size, bias=bias))
+            self.weight_xr.append(SAGEConv(_input_size, hidden_size, bias=bias))
+            self.weight_hr.append(SAGEConv(hidden_size, hidden_size, bias=bias))
+            self.weight_xh.append(SAGEConv(_input_size, hidden_size, bias=bias))
+            self.weight_hh.append(SAGEConv(hidden_size, hidden_size, bias=bias))
+    
+    def forward(self, inp, edgidx, h):
+        h_out = torch.zeros(h.size())
+        for i in range(self.n_layer):
+            _input = inp if i == 0 else h_out[i - 1]
+            z_g = torch.sigmoid(self.weight_xz[i](_input, edgidx) + self.weight_hz[i](h[i], edgidx))
+            r_g = torch.sigmoid(self.weight_xr[i](_input, edgidx) + self.weight_hr[i](h[i], edgidx))
+            h_tilde_g = torch.tanh(self.weight_xh[i](_input, edgidx) + self.weight_hh[i](r_g * h[i], edgidx))
+            h_out[i] = z_g * h[i] + (1 - z_g) * h_tilde_g
+        out = h_out
+        return out, h_out
+
+class InnerProductDecoder(torch.nn.Module):
+    def __init__(self, act=torch.sigmoid, dropout=0.):
+        super(InnerProductDecoder, self).__init__()
+        
+        self.act = act
+        self.dropout = dropout
+    
+    def forward(self, inp):
+        inp = F.dropout(inp, self.dropout, training=self.training)
+        x = torch.transpose(inp, dim0=0, dim1=1)
+        x = torch.mm(inp, x)
+        return self.act(x)
+
+class VGRNN(torch.nn.Module):
+    def __init__(self, x_dim, h_dim, z_dim, n_layers, eps, conv='GCN', bias=False):
+        super(VGRNN, self).__init__()
+        
+        self.x_dim = x_dim
+        self.eps = eps
+        self.h_dim = h_dim
+        self.z_dim = z_dim
+        self.n_layers = n_layers
+        
+        # if conv == 'GCN':
+        self.phi_x = Sequential(Linear(x_dim, h_dim), ReLU())
+        self.phi_z = Sequential(Linear(z_dim, h_dim), ReLU())
+
+        self.enc = GeomSequential('x, edge_index',
+                                  [(GCNConv(h_dim + h_dim, h_dim), 'x, edge_index -> x'),
+                                   ReLU()])
+        self.enc_mean = GCNConv(h_dim, z_dim)
+        self.enc_std = GeomSequential('x, edge_index',
+                                  [(GCNConv(h_dim, z_dim), 'x, edge_index -> x'),
+                                   Softplus()])
+
+        self.prior = Sequential(Linear(h_dim, h_dim), ReLU())
+        self.prior_mean = Sequential(Linear(h_dim, z_dim))
+        self.prior_std = Sequential(Linear(h_dim, z_dim), Softplus())
+
+        self.rnn = Graph_GRU_SAGE(h_dim + h_dim, h_dim, n_layers, bias)
+    
+    def forward(self, x, edge_idx_list, adj_orig_dense_list, hidden_in=None):
+        assert len(adj_orig_dense_list) == len(edge_idx_list)
+        
+        kld_loss = 0
+        nll_loss = 0
+        all_enc_mean, all_enc_std = [], []
+        all_prior_mean, all_prior_std = [], []
+        all_dec_t, all_z_t = [], []
+        
+        if hidden_in is None:
+            h = Variable(torch.zeros(self.n_layers, x.size(1), self.h_dim))
+        else:
+            h = Variable(hidden_in)
+        
+        for t in range(x.size(0)):
+            phi_x_t = self.phi_x(x[t])
+            
+            #encoder
+            enc_t = self.enc(torch.cat([phi_x_t, h[-1]], 1), edge_idx_list[t])
+            enc_mean_t = self.enc_mean(enc_t, edge_idx_list[t])
+            enc_std_t = self.enc_std(enc_t, edge_idx_list[t])
+            
+            #prior
+            prior_t = self.prior(h[-1])
+            prior_mean_t = self.prior_mean(prior_t)
+            prior_std_t = self.prior_std(prior_t)
+            
+            #sampling and reparameterization
+            z_t = self._reparameterized_sample(enc_mean_t, enc_std_t)
+            phi_z_t = self.phi_z(z_t)
+            
+            #decoder
+            dec_t = self.dec(z_t)
+            
+            #recurrence
+            _, h = self.rnn(torch.cat([phi_x_t, phi_z_t], 1), edge_idx_list[t], h)
+            
+            nnodes = adj_orig_dense_list[t].size()[0]
+            enc_mean_t_sl = enc_mean_t[0:nnodes, :]
+            enc_std_t_sl = enc_std_t[0:nnodes, :]
+            prior_mean_t_sl = prior_mean_t[0:nnodes, :]
+            prior_std_t_sl = prior_std_t[0:nnodes, :]
+            dec_t_sl = dec_t[0:nnodes, 0:nnodes]
+            
+            #computing losses
+#             kld_loss += self._kld_gauss_zu(enc_mean_t, enc_std_t)
+            kld_loss += self._kld_gauss(enc_mean_t_sl, enc_std_t_sl, prior_mean_t_sl, prior_std_t_sl)
+            nll_loss += self._nll_bernoulli(dec_t_sl, adj_orig_dense_list[t])
+            
+            all_enc_std.append(enc_std_t_sl)
+            all_enc_mean.append(enc_mean_t_sl)
+            all_prior_mean.append(prior_mean_t_sl)
+            all_prior_std.append(prior_std_t_sl)
+            all_dec_t.append(dec_t_sl)
+            all_z_t.append(z_t)
+        
+        return kld_loss, nll_loss, all_enc_mean, all_prior_mean, h
+    
+    def dec(self, z):
+        outputs = InnerProductDecoder(act=lambda x:x)(z)
+        return outputs
+    
+    def reset_parameters(self, stdv=1e-1):
+        for weight in self.parameters():
+            weight.data.normal_(0, stdv)
+     
+    def _init_weights(self, stdv):
+        pass
+    
+    def _reparameterized_sample(self, mean, std):
+        eps1 = torch.FloatTensor(std.size()).normal_()
+        eps1 = Variable(eps1)
+        return eps1.mul(std).add_(mean)
+    
+    def _kld_gauss(self, mean_1, std_1, mean_2, std_2):
+        num_nodes = mean_1.size()[0]
+        kld_element =  (2 * torch.log(std_2 + self.eps) - 2 * torch.log(std_1 + self.eps) +
+                        (torch.pow(std_1 + self.eps ,2) + torch.pow(mean_1 - mean_2, 2)) / 
+                        torch.pow(std_2 + self.eps ,2) - 1)
+        return (0.5 / num_nodes) * torch.mean(torch.sum(kld_element, dim=1), dim=0)
+    
+    def _kld_gauss_zu(self, mean_in, std_in):
+        num_nodes = mean_in.size()[0]
+        std_log = torch.log(std_in + self.eps)
+        kld_element =  torch.mean(torch.sum(1 + 2 * std_log - mean_in.pow(2) -
+                                            torch.pow(torch.exp(std_log), 2), 1))
+        return (-0.5 / num_nodes) * kld_element
+    
+    def _nll_bernoulli(self, logits, target_adj_dense):
+        temp_size = target_adj_dense.size()[0]
+        temp_sum = target_adj_dense.sum()
+        posw = float(temp_size * temp_size - temp_sum) / temp_sum
+        norm = temp_size * temp_size / float((temp_size * temp_size - temp_sum) * 2)
+        nll_loss_mat = F.binary_cross_entropy_with_logits(input=logits
+                                                          , target=target_adj_dense
+                                                          , pos_weight=posw
+                                                          , reduction='none')
+        nll_loss = -1 * norm * torch.mean(nll_loss_mat, dim=[0,1])
+        return - nll_loss
