f91da12750d5fb25b511b06c20898acf0878eab8
a-mazzetto <92179628+a-mazzetto@users.noreply.github.com>
2023-07-29 18:23:18 +0200

    created function

--- scripts/vgrnn/graph_vae_pred.py
+++ scripts/vgrnn/graph_vae_pred.py
@@ -3,12 +3,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+from torch_geometric.utils import to_dense_adj
 from gnn_data_generation.three_graph_families_dataset import ThreeGraphFamiliesDataset
 from graph_vae import *
+from pvalues.combiners import fisher_pvalues_combiner, min_pvalue_combiner
 
 # %% Load model
 model = torch.load(
-    r"C:\Users\user\git\anomaly-detection\data\trained_on_cpu\vae_demo_2_modes.pt",
+    r"C:\Users\user\git\anomaly-detection\data\trained_on_gpu\vae_demo_simple_3.pt",
     map_location=torch.device('cpu'))
 
 # %%
@@ -16,50 +18,82 @@ model.eval()
 # %% Dataset
 dataset = ThreeGraphFamiliesDataset(use_features=True)
 
-# %% Predict
+# %%
+def score_given_model(model, datum, plots=True):
+    """Assuming model of type Graph VAE"""
+    # Bernoulli probabilities
+    encoded_ld = model.encoder_ld(datum)
+    encoded_ud = model.encoder_ud(datum)
+    mega_sample = model.decoder(
+        torch.stack([model.sampler(*encoded_ld) for _ in range(1000)]),
+        torch.stack([model.sampler(*encoded_ud) for _ in range(1000)]))
+    mega_sample = mega_sample.detach().mean(dim=[0])
+
+    # Probabilities of y_true
+    y_true = to_dense_adj(datum.edge_index)[0]
+    probs = y_true * mega_sample + (1 - mega_sample) * (1 - y_true)
+
+    # Should be the same as
+    bernoulli_dist = dist.Bernoulli(probs=mega_sample)
+    probs_2 = bernoulli_dist.log_prob(y_true).exp()
+    # assert equality
+    assert torch.allclose(probs, probs_2)
+
+    # I keep the probs only in the region that were one for either matrix
+    locs = (mega_sample > 0.5).type(torch.int).logical_or(y_true)
+    selected_probs = probs[0][locs[0]]
+
+    # Calculate p-values
+    if len(selected_probs) > 0:
+        fisher_score = fisher_pvalues_combiner(selected_probs.flatten().numpy())
+        min_score = min_pvalue_combiner(selected_probs.flatten().numpy())
+    else:
+        fisher_score = 0
+        min_score = 0
+
+    if plots:
+        print(f"Fisher p-value: {fisher_score}")
+        print(f"Min p-value: {min_score}")
+
+        n_true = y_true.sum().numpy().astype(int)
+        n_pred = (mega_sample > 0.25).type(torch.int).sum().numpy().astype(int)
+        n_common = y_true.logical_and((mega_sample > 0.25).type(torch.int)).sum().numpy().astype(int)
+        print(f"Aimed {n_common}, missed {n_true - n_common}, misselected {n_pred - n_common}")
+
+        fig, ax = plt.subplots(1, 3)
+        ax[0].imshow(y_true)
+        ax[0].set_title("True Adjacency")
+        ax[1].imshow(mega_sample)
+        ax[1].set_title("Model probability")
+        ax[2].imshow((mega_sample > 0.25).type(torch.float))
+        ax[2].set_title("Model probability thresholded")
+        fig.tight_layout()
+        fig.show()
+
+    return fisher_score, min_score
+
+# %% Calculate for one example
 selected_class = dataset[dataset.y == 0]
 idx_choice = np.random.choice(len(selected_class))
 test_datum = selected_class[idx_choice]
-_, _, _, y_true = model(test_datum)
+score_given_model(model, test_datum)
 
-# %%
-# Use Poisson Binomial Distribution
-import torch.distributions as dist
-mega_sample = model.decoder(
-    dist.Normal(*model.encoder_ld(test_datum)).sample((1000,)),
-    dist.Normal(*model.encoder_ud(test_datum)).sample((1000,)))
-mega_sample = mega_sample.mean(dim=[0])
-# Probability of y_true given the model
-probs = y_true * mega_sample + (1 - mega_sample) * (1 - y_true)
+# %% Check for all
+results = np.ndarray((0, 3))
+for idx, datum in enumerate(dataset):
+    print(f"{idx}th element")
+    results = np.vstack((results,
+        np.array([*score_given_model(model, datum, plots=False),
+                  datum.y.item()]).reshape(1, -1))
+    )
 
-# Should be the same as
-bernoulli_dist = dist.Bernoulli(probs=mega_sample)
-probs_2 = bernoulli_dist.log_prob(y_true).exp()
-# assert equality
-print(torch.allclose(probs, probs_2))
+plt.hist(results[results[:, -1] == 0][:, 0], density=True, alpha=0.3, range=[0, 1], bins=50)
+plt.hist(results[results[:, -1] == 1][:, 0], density=True, alpha=0.3, range=[0, 1], bins=50)
+plt.title("Fisher")
+plt.show()
 
-# I keep the probs only in the regione that were one for either matrix
-locs = (mega_sample > 0.5).type(torch.int).logical_or(y_true)
-selected_probs = probs[0][locs[0]]
-
-# Calculate united p-value
-from pvalues.combiners import fisher_pvalues_combiner, min_pvalue_combiner
-
-print(f"Fisher p-value: {fisher_pvalues_combiner(selected_probs.flatten().numpy())}")
-print(f"Min p-value: {min_pvalue_combiner(selected_probs.flatten().numpy())}")
-# %% Plot adjacency matrices
-fig, ax = plt.subplots(1, 3)
-ax[0].imshow(y_true[0])
-ax[0].set_title("True Adjacency")
-ax[1].imshow(mega_sample)
-ax[1].set_title("Model probability")
-ax[2].imshow((mega_sample > 0.25).type(torch.float))
-ax[2].set_title("Model probability thresholded")
-
-# %%
-n_true = y_true[0].sum().numpy().astype(int)
-n_pred = (mega_sample > 0.25).type(torch.int).sum().numpy().astype(int)
-n_common = y_true[0].logical_and((mega_sample > 0.25).type(torch.int)).sum().numpy().astype(int)
-print(f"Aimed {n_common}, missed {n_true - n_common}, misselected {n_pred - n_common}")
+plt.hist(results[results[:, -1] == 0][:, 1], density=True, alpha=0.3, range=[0, 1], bins=50)
+plt.hist(results[results[:, -1] == 1][:, 1], density=True, alpha=0.3, range=[0, 1], bins=50)
+plt.title("Min")
 
 # %%
