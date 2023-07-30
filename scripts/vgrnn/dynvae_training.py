b293347dd2577ae4c657582681e2c2ceb276dbd4
a-mazzetto <92179628+a-mazzetto@users.noreply.github.com>
2023-07-30 12:30:49 +0200

    created DynVAE

---
+++ scripts/vgrnn/dynvae_training.py
@@ -0,0 +1,68 @@
+"""Demo training of VGRNN with custom data"""
+# %% Imports
+import os
+import argparse
+import torch
+from torch_geometric.data import Batch, Data
+from torch_geometric.loader import DataLoader
+from vgrnn.dynvae import DynVAE
+from gnn_data_generation.vgrnn_test_dataset import VGRNNTestDataset
+from gnn.early_stopping import EarlyStopping
+from gnn.train_loops import vgrnn_train_loop, plot_vgrnn_training_result
+
+# %% Initialization
+try:
+    parser = argparse.ArgumentParser(description="Classify three graph families",
+                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-feat", "--use_node_features", action="store_false", help="Use node features in my dataset")
+    parser.add_argument("-e", "--epochs", type=int, default=10, help="Number of epochs")
+    parser.add_argument("-p", "--patience", type=int, default=10, help="Patience")
+    parser.add_argument("-pf", "--print_freq", type=int, default=1, help="Patience")
+
+    args = parser.parse_args()
+    config = vars(args)
+
+    USE_NODE_FEATURES = config["use_node_features"]
+    NUM_EPOCHS = config["epochs"]
+    PATIENCE = config["patience"]
+    PRINT_FREQ = config["print_freq"]
+    print(config)
+except:
+    print("Setting default values, argparser failed!")
+    USE_NODE_FEATURES = True
+    NUM_EPOCHS = 2
+    PATIENCE = 2
+    PRINT_FREQ = 1
+
+# %% Load dataset
+dataset = VGRNNTestDataset(use_features=USE_NODE_FEATURES)
+loader = DataLoader(dataset, batch_size=32)
+train_idx, test_idx = torch.utils.data.random_split(
+    dataset, (0.7, 0.3), torch.Generator().manual_seed(0))
+
+train_dataset = [dataset[_idx] for _idx in train_idx.indices]
+test_dataset = [dataset[_idx] for _idx in test_idx.indices]
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32)
+
+# %% Model
+
+dynvae_model = DynVAE(x_dim=dataset.x.size(-1), h_dim=32, z_dim=16, n_layers=5, eps=1e-10, bias=True)
+
+optimizer = torch.optim.Adam(dynvae_model.parameters(), lr=1e-2)
+
+# %% Training
+
+history = vgrnn_train_loop(model=dynvae_model, optimizer=optimizer, num_epochs=NUM_EPOCHS,
+                           train_loader=train_dataset, val_loader=test_dataset,
+                           early_stopping=EarlyStopping(patience=PATIENCE),
+                           best_model_path="./data/vgrnn_demo_model.pt", print_freq=PRINT_FREQ)
+
+# %% Plotting
+
+fig = plot_vgrnn_training_result(history=history)
+os.makedirs("./plots", exist_ok=True)
+fig.savefig("./plots/vgrnn_training.pdf")
+
+# %%
---
+++ vgrnn/dynvae.py
@@ -0,0 +1,201 @@
+"""VGRNN imlementation following https://arxiv.org/abs/1908.09710
+Adapted for directed graphs
+Extended for multi-model prior"""
+import torch
+from torch.nn import Sequential, Linear, ReLU, Softplus, ModuleList
+from torch_geometric.nn.conv import GCNConv, GINConv
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+class Graph_GRU(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, n_layer, bias=True):
+        super(Graph_GRU, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.n_layer = n_layer
+        
+        # GRU weights
+        self.weight_xz = ModuleList()
+        self.weight_hz = ModuleList()
+        self.weight_xr = ModuleList()
+        self.weight_hr = ModuleList()
+        self.weight_xh = ModuleList()
+        self.weight_hh = ModuleList()
+        
+        for i in range(self.n_layer):
+            _input_size = input_size if i == 0 else hidden_size
+            self.weight_xz.append(GINConv(Linear(_input_size, hidden_size, bias=bias)))
+            self.weight_hz.append(GINConv(Linear(hidden_size, hidden_size, bias=bias)))
+            self.weight_xr.append(GINConv(Linear(_input_size, hidden_size, bias=bias)))
+            self.weight_hr.append(GINConv(Linear(hidden_size, hidden_size, bias=bias)))
+            self.weight_xh.append(GINConv(Linear(_input_size, hidden_size, bias=bias)))
+            self.weight_hh.append(GINConv(Linear(hidden_size, hidden_size, bias=bias)))
+    
+    def forward(self, inp, edgidx, h):
+        h_out = torch.zeros(h.size()).to(h.device)
+        for i in range(self.n_layer):
+            _input = inp if i == 0 else h_out[i - 1]
+            z_g = torch.sigmoid(self.weight_xz[i](_input, edgidx) + self.weight_hz[i](h[i], edgidx))
+            r_g = torch.sigmoid(self.weight_xr[i](_input, edgidx) + self.weight_hr[i](h[i], edgidx))
+            h_tilde_g = torch.tanh(self.weight_xh[i](_input, edgidx) + self.weight_hh[i](r_g * h[i], edgidx))
+            h_out[i] = z_g * h[i] + (1 - z_g) * h_tilde_g
+        out = h_out
+        return out, h_out
+
+class InnerProductDecoder(torch.nn.Module):
+    def __init__(self, act=torch.sigmoid, dropout=0.):
+        super(InnerProductDecoder, self).__init__()
+        
+        self.act = act
+        self.dropout = dropout
+    
+    def forward(self, inp_src, inp_dst):
+        inp_src = F.dropout(inp_src, self.dropout, training=self.training)
+        inp_dst = F.dropout(inp_dst, self.dropout, training=self.training)
+        inp_dst_t = torch.transpose(inp_dst, dim0=0, dim1=1)
+        x = torch.mm(inp_src, inp_dst_t)
+        return self.act(x)
+
+class DynVAE(torch.nn.Module):
+    def __init__(self, x_dim, h_dim, z_dim, n_layers, eps, bias=False):
+        super(DynVAE, self).__init__()
+        
+        self.x_dim = x_dim
+        self.eps = eps
+        self.h_dim = h_dim
+        self.z_dim = z_dim
+        self.n_layers = n_layers
+        
+        # Functions for GRU recurrence
+        self.phi_x = Sequential(Linear(x_dim, h_dim, bias=bias), ReLU())
+        self.phi_z = Sequential(Linear(z_dim + z_dim, h_dim, bias=bias), ReLU())
+        self.rnn = Graph_GRU(h_dim + h_dim, h_dim, n_layers, bias)
+
+        # Encoder: 2-layered GIN
+        self.enc_src = GINConv(Sequential(Linear(h_dim + h_dim, h_dim), ReLU()))
+        self.enc_src_mean = GCNConv(h_dim, z_dim)
+        self.enc_src_std = GINConv(Sequential(Linear(h_dim, z_dim), Softplus()))
+
+        self.enc_dst = GINConv(Sequential(Linear(h_dim + h_dim, h_dim), ReLU()))
+        self.enc_dst_mean = GCNConv(h_dim, z_dim)
+        self.enc_dst_std = GINConv(Sequential(Linear(h_dim, z_dim), Softplus()))
+
+        # Prior: 2-layered MLP
+        self.prior_src = Sequential(Linear(h_dim, h_dim), ReLU())
+        self.prior_src_mean = Sequential(Linear(h_dim, z_dim))
+        self.prior_src_std = Sequential(Linear(h_dim, z_dim), Softplus())
+
+        self.prior_dst = Sequential(Linear(h_dim, h_dim), ReLU())
+        self.prior_dst_mean = Sequential(Linear(h_dim, z_dim))
+        self.prior_dst_std = Sequential(Linear(h_dim, z_dim), Softplus())
+    
+    def forward(self, x, edge_idx_list, adj_orig_dense_list, hidden_in=None):
+        assert len(adj_orig_dense_list) == len(edge_idx_list)
+        
+        kld_loss = 0
+        nll_loss = 0
+        all_dec_t = []
+        
+        if hidden_in is None:
+            h = torch.zeros(self.n_layers, x.size(1), self.h_dim, requires_grad=True).to(x.device)
+        else:
+            h = hidden_in.clone().detach().requires_grad_(True).to(x.device)
+        
+        for t in range(x.size(0)):
+            phi_x_t = self.phi_x(x[t])
+
+            #encoder
+            enc_src_t = self.enc_src(torch.cat([phi_x_t, h[-1]], 1), edge_idx_list[t])
+            enc_src_mean_t = self.enc_src_mean(enc_src_t, edge_idx_list[t])
+            enc_src_std_t = self.enc_src_std(enc_src_t, edge_idx_list[t])
+
+            enc_dst_t = self.enc_dst(torch.cat([phi_x_t, h[-1]], 1), edge_idx_list[t])
+            enc_dst_mean_t = self.enc_dst_mean(enc_dst_t, edge_idx_list[t])
+            enc_dst_std_t = self.enc_dst_std(enc_dst_t, edge_idx_list[t])
+            
+            #prior
+            prior_src_t = self.prior_src(h[-1])
+            prior_src_mean_t = self.prior_src_mean(prior_src_t)
+            prior_src_std_t = self.prior_src_std(prior_src_t)
+
+            prior_dst_t = self.prior_dst(h[-1])
+            prior_dst_mean_t = self.prior_dst_mean(prior_dst_t)
+            prior_dst_std_t = self.prior_dst_std(prior_dst_t)
+            
+            #sampling and reparameterization
+            z_src_t = self._reparameterized_sample(enc_src_mean_t, enc_src_std_t)
+            z_dst_t = self._reparameterized_sample(enc_dst_mean_t, enc_dst_std_t)
+            phi_z_t = self.phi_z(torch.cat([z_src_t, z_dst_t], 1))
+            
+            #decoder
+            dec_t = self.dec(z_src_t, z_dst_t)
+            
+            #recurrence
+            _, h = self.rnn(torch.cat([phi_x_t, phi_z_t], 1), edge_idx_list[t], h)
+            
+            nnodes = adj_orig_dense_list[t].size()[0]
+
+            enc_src_mean_t_sl = enc_src_mean_t[0:nnodes, :]
+            enc_src_std_t_sl = enc_src_std_t[0:nnodes, :]
+
+            enc_dst_mean_t_sl = enc_dst_mean_t[0:nnodes, :]
+            enc_dst_std_t_sl = enc_dst_std_t[0:nnodes, :]
+
+            prior_src_mean_t_sl = prior_src_mean_t[0:nnodes, :]
+            prior_src_std_t_sl = prior_src_std_t[0:nnodes, :]
+
+            prior_dst_mean_t_sl = prior_dst_mean_t[0:nnodes, :]
+            prior_dst_std_t_sl = prior_dst_std_t[0:nnodes, :]
+
+            dec_t_sl = dec_t[0:nnodes, 0:nnodes]
+            
+            #computing losses
+            kld_loss += self._kld_gauss(enc_src_mean_t_sl, enc_src_std_t_sl, prior_src_mean_t_sl, prior_src_std_t_sl) + \
+                self._kld_gauss(enc_dst_mean_t_sl, enc_dst_std_t_sl, prior_dst_mean_t_sl, prior_dst_std_t_sl)
+            nll_loss += self._nll_bernoulli(dec_t_sl, adj_orig_dense_list[t])
+
+            all_dec_t.append(dec_t_sl)
+        
+        return kld_loss, nll_loss, None, None, None, all_dec_t
+    
+    def dec(self, z_src, z_dst):
+        outputs = InnerProductDecoder(act=lambda x:x)(z_src, z_dst)
+        return outputs
+    
+    def reset_parameters(self, stdv=1e-1):
+        for weight in self.parameters():
+            weight.data.normal_(0, stdv)
+     
+    def _init_weights(self, stdv):
+        pass
+    
+    def _reparameterized_sample(self, mean, std):
+        eps1 = torch.FloatTensor(std.size()).normal_()
+        eps1 = Variable(eps1).to(mean.device)
+        return eps1.mul(std).add_(mean)
+    
+    def _kld_gauss(self, mean_1, std_1, mean_2, std_2):
+        num_nodes = mean_1.size()[0]
+        kld_element =  (2 * torch.log(std_2 + self.eps) - 2 * torch.log(std_1 + self.eps) +
+                        (torch.pow(std_1 + self.eps ,2) + torch.pow(mean_1 - mean_2, 2)) / 
+                        torch.pow(std_2 + self.eps ,2) - 1)
+        return (0.5 / num_nodes) * torch.mean(torch.sum(kld_element, dim=1), dim=0)
+    
+    def _kld_gauss_zu(self, mean_in, std_in):
+        num_nodes = mean_in.size()[0]
+        std_log = torch.log(std_in + self.eps)
+        kld_element =  torch.mean(torch.sum(1 + 2 * std_log - mean_in.pow(2) -
+                                            torch.pow(torch.exp(std_log), 2), 1))
+        return (-0.5 / num_nodes) * kld_element
+    
+    def _nll_bernoulli(self, logits, target_adj_dense):
+        temp_size = target_adj_dense.size()[0]
+        temp_sum = target_adj_dense.sum()
+        posw = float(temp_size * temp_size - temp_sum) / temp_sum
+        norm = temp_size * temp_size / float((temp_size * temp_size - temp_sum) * 2)
+        nll_loss_mat = F.binary_cross_entropy_with_logits(input=logits,
+                                                          target=target_adj_dense,
+                                                          pos_weight=posw,
+                                                          reduction='none')
+        nll_loss = -1 * norm * torch.mean(nll_loss_mat, dim=[0,1])
+        return - nll_loss
