78bf85117db67c9c96013fb0922bcd35754621fc
a-mazzetto <92179628+a-mazzetto@users.noreply.github.com>
2023-08-06 18:21:47 +0200

    print memory

--- cuda/cuda_utils.py
+++ cuda/cuda_utils.py
@@ -7,10 +7,11 @@ def select_device():
         selected_gpu_memory = 1e8
         for n_gpu in range(torch.cuda.device_count()):
             current_gpu_memory = torch.cuda.memory_usage(n_gpu)
+            print(f"GPU {n_gpu} MEM {current_gpu_memory}")
             if current_gpu_memory < selected_gpu_memory:
                 selected_gpu_memory = current_gpu_memory
                 selected_gpu = n_gpu
-        print(f"Selecting GPU number {selected_gpu}")
+        print(f"Selected GPU number {selected_gpu}")
         return torch.device("cuda", selected_gpu)
     else:
         return torch.device("cpu")
