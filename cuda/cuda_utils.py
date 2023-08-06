6576ed644d8de13e80daae29a27e6d453bc16f2a
a-mazzetto <92179628+a-mazzetto@users.noreply.github.com>
2023-08-06 18:29:07 +0200

    final version cuda selection

--- cuda/cuda_utils.py
+++ cuda/cuda_utils.py
@@ -4,12 +4,11 @@ import torch
 def select_device():
     """Select leats busy GPU or CPU if no GPU available"""
     if torch.cuda.is_available():
-        selected_gpu_memory = 1e8
+        selected_gpu_memory = 0
         for n_gpu in range(torch.cuda.device_count()):
             try:
                 current_gpu_memory = torch.cuda.mem_get_info(n_gpu)[0]
-                print(f"GPU {n_gpu} MEM {current_gpu_memory}")
-                if current_gpu_memory < selected_gpu_memory:
+                if current_gpu_memory > selected_gpu_memory:
                     selected_gpu_memory = current_gpu_memory
                     selected_gpu = n_gpu
             except RuntimeError as e:
