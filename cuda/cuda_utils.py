8ea2c60c5154fe63c2e994952916be79cead6268
a-mazzetto <92179628+a-mazzetto@users.noreply.github.com>
2023-08-06 18:25:21 +0200

    catch runtime error

--- cuda/cuda_utils.py
+++ cuda/cuda_utils.py
@@ -6,11 +6,14 @@ def select_device():
     if torch.cuda.is_available():
         selected_gpu_memory = 1e8
         for n_gpu in range(torch.cuda.device_count()):
-            current_gpu_memory = torch.cuda.mem_get_info(n_gpu)[0]
-            print(f"GPU {n_gpu} MEM {current_gpu_memory}")
-            if current_gpu_memory < selected_gpu_memory:
-                selected_gpu_memory = current_gpu_memory
-                selected_gpu = n_gpu
+            try:
+                current_gpu_memory = torch.cuda.mem_get_info(n_gpu)[0]
+                print(f"GPU {n_gpu} MEM {current_gpu_memory}")
+                if current_gpu_memory < selected_gpu_memory:
+                    selected_gpu_memory = current_gpu_memory
+                    selected_gpu = n_gpu
+            except RuntimeError as e:
+                print(f"CUDA:{n_gpu} returned {str(e)}")
         print(f"Selected GPU number {selected_gpu}")
         return torch.device("cuda", selected_gpu)
     else:
