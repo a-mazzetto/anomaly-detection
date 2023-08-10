aa40857e7e8bbd91ad62c2dac9884ff45109bb53
a-mazzetto <92179628+a-mazzetto@users.noreply.github.com>
2023-08-10 18:55:17 +0200

    grapg vae tidy script

--- gnn/vae.py
+++ gnn/vae.py
@@ -1,107 +1,11 @@
-"""Graph Variational Autoencoder"""
-
+"""Graph Variational Auto-encoder"""
 # %% Imports
-import os
-import argparse
 import torch
 import torch.distributions as dist
-from torch_geometric.loader import DataLoader
 from torch_geometric.nn import GIN, global_mean_pool
 from torch_geometric.utils import to_dense_adj
-from gnn_data_generation.three_graph_families_dataset import ThreeGraphFamiliesDataset
-from gnn_data_generation.one_graph_family_dataset import OneGraphFamilyDataset
-
-from gnn.train_loops import vae_training_loop, plot_vae_training_results
-from gnn.early_stopping import EarlyStopping
-from cuda.cuda_utils import select_device
-
-# %% Initialization
-try:
-    parser = argparse.ArgumentParser(description="Graph VAE example",
-                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-gdat", "--giant_dataset", action="store_false", help="Use giant dataset?")
-    parser.add_argument("-feat", "--use_node_features", action="store_false", help="Use node features in my dataset")
-    parser.add_argument("-nbatch", "--batch_size", type=int, default=32, help="Batch size")
-    parser.add_argument("-optstep", "--optimizer_step", type=float, default=5e-4, help="Optimizer step")
-    parser.add_argument("-optdecay", "--optimizer_decay", type=float, default=5e-4, help="Optimizer step")
-
-    parser.add_argument("-e", "--epochs", type=int, default=10, help="Number of epochs")
-    parser.add_argument("-p", "--patience", type=int, default=10, help="Patience")
-    parser.add_argument("-pf", "--print_freq", type=int, default=1, help="Patience")
-
-    parser.add_argument("-lat", "--latent_dim", type=int, default=16, help="Latent dimension")
-    parser.add_argument("-hid", "--hidden_dim", type=int, default=54, help="Hidden dimension")
-    parser.add_argument("-ngl", "--n_gin_layers", type=int, default=10, help="Number GIN layers")
-    parser.add_argument("-modes", "--n_modes", type=int, default=0, help="Mixture of Gaussians?")
-
-    parser.add_argument("-bgenc", "--b_graph_enc", action="store_false", help="Use graph encoder?")
-    parser.add_argument("-pwgt", "--pos_weight", type=float, default=None, help="Positive label weight")
-
-    args = parser.parse_args()
-    config = vars(args)
-
-    USE_GIANT_DATASET = config["giant_dataset"]
-    USE_NODE_FEATURES = config["use_node_features"]
-    N_BATCH = config["batch_size"]
-    OPT_STEP = config["optimizer_step"]
-    OPT_DECAY = config["optimizer_decay"]
-
-    NUM_EPOCHS = config["epochs"]
-    PATIENCE = config["patience"]
-    PRINT_FREQ = config["print_freq"]
-
-    LATENT_DIM = config["latent_dim"]
-    HIDDEN_DIM = config["hidden_dim"]
-    N_GIN_LAYERS = config["n_gin_layers"]
-    N_PRIOR_MODES = config["n_modes"]
-
-    USE_GRAPH_ENC = config["b_graph_enc"]
-    POS_WEIGHT = config["pos_weight"]
-
-    print(config)
-except:
-    print("Setting default values, argparser failed!")
-
-    USE_GIANT_DATASET = False
-    USE_NODE_FEATURES = True
-    N_BATCH = 32
-    OPT_STEP = 1e-3
-    OPT_DECAY = 1e-5
-
-    NUM_EPOCHS = 20
-    PATIENCE = 20
-    PRINT_FREQ = 1
-
-    LATENT_DIM = 16
-    HIDDEN_DIM = 54
-    N_GIN_LAYERS = 10
-    N_PRIOR_MODES = 3
-
-    USE_GRAPH_ENC = True
-    POS_WEIGHT = None
-
-SIMPLE_PRIOR = N_PRIOR_MODES < 1
-
-# %% Dataset
-if USE_GIANT_DATASET:
-    dataset = OneGraphFamilyDataset(use_features=USE_NODE_FEATURES)
-else:
-    dataset = ThreeGraphFamiliesDataset(use_features=USE_NODE_FEATURES)
-    # Use only one class
-    dataset = dataset[dataset.y == 0]
-
-train_idx, test_idx = torch.utils.data.random_split(
-    dataset, (0.7, 0.3), torch.Generator().manual_seed(0))
-
-train_dataset = [dataset[_idx] for _idx in train_idx.indices]
-test_dataset = [dataset[_idx] for _idx in test_idx.indices]
-
-train_loader = DataLoader(train_dataset, batch_size=N_BATCH, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=N_BATCH)
 
 # %% Prior distribution
-latent_dim = 8
-
 def get_prior(device, num_modes, latent_dim):
     """
     This function should create an instance of a MixtureSameFamily distribution
@@ -120,10 +24,13 @@ def get_prior(device, num_modes, latent_dim):
         component_distribution=dist.Independent(dist.Normal(loc=loc, scale=scale), 1))
     return prior
 
-# Simple Gaussian
-# prior = dist.Normal(loc=torch.zeros(latent_dim), scale=1.)
-# Gaussian Mixture
-# prior = get_prior(num_modes=N_PRIOR_MODES, latent_dim=LATENT_DIM, device=torch.device("cpu"))
+if __name__ == "__main__":
+    latent_dim = 8
+    prior = get_prior(torch.device("cpu"), 3, latent_dim)
+    # Simple Gaussian
+    prior = dist.Normal(loc=torch.zeros(latent_dim), scale=1.)
+    # Gaussian Mixture
+    prior = get_prior(num_modes=3, latent_dim=latent_dim, device=torch.device("cpu"))
 
 # %% Encoder
 class Encoder(torch.nn.Module):
@@ -142,8 +49,11 @@ class Encoder(torch.nn.Module):
             hidden += addition
         return self.mean(hidden), self.std(hidden)
 
-encoder = Encoder()
-loc, scale = encoder(dataset[0])
+if __name__ == "__main__":
+    from gnn_data_generation.three_graph_families_dataset import ThreeGraphFamiliesDataset
+    dataset = ThreeGraphFamiliesDataset(use_features=True)
+    encoder = Encoder()
+    loc, scale = encoder(dataset[0])
 
 # %% Graph Encoder
 class GraphEncoder(torch.nn.Module):
@@ -155,31 +65,17 @@ class GraphEncoder(torch.nn.Module):
     def forward(self, data):
         hidden = self.gin(data.x, data.edge_index)
         hidden_pool = global_mean_pool(hidden, data.batch)
-        hidden = hidden_pool[data.batch]
+        if data.batch is not None:
+            hidden = hidden_pool[data.batch]
+        else:
+            hidden = hidden_pool
         return hidden
 
-graph_encoder = GraphEncoder()
-encoding = graph_encoder(dataset[0])
+if __name__ == "__main__":
+    graph_encoder = GraphEncoder()
+    encoding = graph_encoder(dataset[0])
 
 # %% Decoder
-
-class InnerProductDecoder(torch.nn.Module):
-    def __init__(self, logits):
-        super().__init__()
-        self.logits = logits
-        self.dropout = torch.nn.Dropout(p=0.2)
-    
-    def forward(self, inp):
-        inp = self.dropout(inp)
-        x = torch.transpose(inp, dim0=-2, dim1=-1)
-        x = torch.matmul(inp, x)
-        if self.logits:
-            return x
-        else:
-            return torch.sigmoid(x)
-
-# decoder = InnerProductDecoder()
-# decoder(prior.sample(torch.Size([2])))
 
 class InnerProductDecoder2(torch.nn.Module):
     def __init__(self, logits):
@@ -197,6 +93,11 @@ class InnerProductDecoder2(torch.nn.Module):
         else:
             return torch.sigmoid(x)
 
+if __name__ == "__main__":
+    decoder = InnerProductDecoder2(logits=True)
+    decoder(prior.sample(torch.Size([2])),
+            prior.sample(torch.Size([2])))
+    
 # %% VAE
 class VAE(torch.nn.Module):
     def __init__(self, device, latent_dim=8, hidden_dim=16, n_gin_layers=10, prior_modes=0,
@@ -206,7 +107,7 @@ class VAE(torch.nn.Module):
         # prior
         self.simple_prior = prior_modes < 1
         self.prior = dist.Normal(loc=torch.zeros(latent_dim).to(device), scale=1.) if \
-            self.simple_prior else get_prior(device, num_modes=prior_modes, latent_dim=LATENT_DIM)
+            self.simple_prior else get_prior(device, num_modes=prior_modes, latent_dim=latent_dim)
 
         # encoder, decoder
         self.encoder_ld = Encoder(latent_dim=latent_dim, hidden_dim=hidden_dim, n_gin_layers=n_gin_layers)
@@ -290,29 +191,12 @@ class VAE(torch.nn.Module):
         kld_element =  torch.sum(1 + 2 * std_log - loc.pow(2) - torch.pow(torch.exp(std_log), 2))
         return -0.5 * kld_element
 
-# vae = VAE(prior_modes=N_PRIOR_MODES, latent_dim=LATENT_DIM, hidden_dim=HIDDEN_DIM, device=torch.device("cpu"))
-# vae_call = vae(dataset[0])
-# vae_call_batch = vae(next(iter(train_loader)))
-
-# %% Kick training
 if __name__ == "__main__":
-    device = select_device()
-
-    model = VAE(device=device, prior_modes=N_PRIOR_MODES, latent_dim=LATENT_DIM, hidden_dim=HIDDEN_DIM,
-                n_gin_layers=N_GIN_LAYERS, graph_enc=USE_GRAPH_ENC, pos_weight=POS_WEIGHT)
-    optimizer = torch.optim.Adam(model.parameters(), lr=OPT_STEP, weight_decay=OPT_DECAY)
-
-    history = vae_training_loop(model=model, optimizer=optimizer, num_epochs=NUM_EPOCHS,
-        train_dl=train_loader, val_dl=test_loader, early_stopping=EarlyStopping(patience=PATIENCE),
-        best_model_path="./data/vae_demo.pt", print_freq=PRINT_FREQ, device=device)
-# %% Validate
-
-# vae.eval()
-# example = vae(dataset[10])
-# %% Plotting
-if __name__ == "__main__":
-    fig = plot_vae_training_results(history=history)
-    os.makedirs("./plots", exist_ok=True)
-    fig.savefig("./plots/vae_training.pdf")
+    from torch_geometric.loader import DataLoader
+    loader = DataLoader(dataset, batch_size=32)
+    model = VAE(device=torch.device("cpu"), prior_modes=0, latent_dim=2, hidden_dim=12,
+                n_gin_layers=3, graph_enc=True, pos_weight=1.)
+    model(dataset[0])
+    model(next(iter(loader)))
 
 # %%
---
+++ scripts/vae/vae_training.py
@@ -0,0 +1,115 @@
+"""Graph Variational Autoencoder"""
+
+# %% Imports
+import os
+import argparse
+import torch
+from torch_geometric.loader import DataLoader
+from gnn.vae import VAE
+from gnn_data_generation.three_graph_families_dataset import ThreeGraphFamiliesDataset
+from gnn_data_generation.one_graph_family_dataset import OneGraphFamilyDataset
+
+from gnn.train_loops import vae_training_loop, plot_vae_training_results
+from gnn.early_stopping import EarlyStopping
+from cuda.cuda_utils import select_device
+
+# %% Initialization
+try:
+    parser = argparse.ArgumentParser(description="Graph VAE example",
+                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-gdat", "--giant_dataset", action="store_false", help="Use giant dataset?")
+    parser.add_argument("-feat", "--use_node_features", action="store_false", help="Use node features in my dataset")
+    parser.add_argument("-nbatch", "--batch_size", type=int, default=32, help="Batch size")
+    parser.add_argument("-optstep", "--optimizer_step", type=float, default=5e-4, help="Optimizer step")
+    parser.add_argument("-optdecay", "--optimizer_decay", type=float, default=5e-4, help="Optimizer step")
+
+    parser.add_argument("-e", "--epochs", type=int, default=10, help="Number of epochs")
+    parser.add_argument("-p", "--patience", type=int, default=10, help="Patience")
+    parser.add_argument("-pf", "--print_freq", type=int, default=1, help="Patience")
+
+    parser.add_argument("-lat", "--latent_dim", type=int, default=16, help="Latent dimension")
+    parser.add_argument("-hid", "--hidden_dim", type=int, default=54, help="Hidden dimension")
+    parser.add_argument("-ngl", "--n_gin_layers", type=int, default=10, help="Number GIN layers")
+    parser.add_argument("-modes", "--n_modes", type=int, default=0, help="Mixture of Gaussians?")
+
+    parser.add_argument("-bgenc", "--b_graph_enc", action="store_false", help="Use graph encoder?")
+    parser.add_argument("-pwgt", "--pos_weight", type=float, default=None, help="Positive label weight")
+
+    args = parser.parse_args()
+    config = vars(args)
+
+    USE_GIANT_DATASET = config["giant_dataset"]
+    USE_NODE_FEATURES = config["use_node_features"]
+    N_BATCH = config["batch_size"]
+    OPT_STEP = config["optimizer_step"]
+    OPT_DECAY = config["optimizer_decay"]
+
+    NUM_EPOCHS = config["epochs"]
+    PATIENCE = config["patience"]
+    PRINT_FREQ = config["print_freq"]
+
+    LATENT_DIM = config["latent_dim"]
+    HIDDEN_DIM = config["hidden_dim"]
+    N_GIN_LAYERS = config["n_gin_layers"]
+    N_PRIOR_MODES = config["n_modes"]
+
+    USE_GRAPH_ENC = config["b_graph_enc"]
+    POS_WEIGHT = config["pos_weight"]
+
+    print(config)
+except:
+    print("Setting default values, argparser failed!")
+
+    USE_GIANT_DATASET = False
+    USE_NODE_FEATURES = True
+    N_BATCH = 32
+    OPT_STEP = 1e-3
+    OPT_DECAY = 1e-5
+
+    NUM_EPOCHS = 20
+    PATIENCE = 20
+    PRINT_FREQ = 1
+
+    LATENT_DIM = 16
+    HIDDEN_DIM = 54
+    N_GIN_LAYERS = 10
+    N_PRIOR_MODES = 3
+
+    USE_GRAPH_ENC = True
+    POS_WEIGHT = None
+
+SIMPLE_PRIOR = N_PRIOR_MODES < 1
+
+# %% Dataset
+if USE_GIANT_DATASET:
+    dataset = OneGraphFamilyDataset(use_features=USE_NODE_FEATURES)
+else:
+    dataset = ThreeGraphFamiliesDataset(use_features=USE_NODE_FEATURES)
+    # Use only one class
+    dataset = dataset[dataset.y == 0]
+
+train_idx, test_idx = torch.utils.data.random_split(
+    dataset, (0.7, 0.3), torch.Generator().manual_seed(0))
+
+train_dataset = [dataset[_idx] for _idx in train_idx.indices]
+test_dataset = [dataset[_idx] for _idx in test_idx.indices]
+
+train_loader = DataLoader(train_dataset, batch_size=N_BATCH, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=N_BATCH)
+
+# %% Kick training, save best model and history plot
+device = select_device()
+
+model = VAE(device=device, prior_modes=N_PRIOR_MODES, latent_dim=LATENT_DIM, hidden_dim=HIDDEN_DIM,
+            n_gin_layers=N_GIN_LAYERS, graph_enc=USE_GRAPH_ENC, pos_weight=POS_WEIGHT)
+optimizer = torch.optim.Adam(model.parameters(), lr=OPT_STEP, weight_decay=OPT_DECAY)
+
+history = vae_training_loop(model=model, optimizer=optimizer, num_epochs=NUM_EPOCHS,
+    train_dl=train_loader, val_dl=test_loader, early_stopping=EarlyStopping(patience=PATIENCE),
+    best_model_path="./data/vae_demo.pt", print_freq=PRINT_FREQ, device=device)
+
+fig = plot_vae_training_results(history=history)
+os.makedirs("./plots", exist_ok=True)
+fig.savefig("./plots/vae_training.pdf")
+
+# %%
