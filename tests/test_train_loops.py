6ad5a07ff7c18f3aa8a7a8898caba50829841eaa
a-mazzetto <92179628+a-mazzetto@users.noreply.github.com>
2023-08-10 20:00:26 +0200

    vae tests

--- tests/test_train_loops.py
+++ tests/test_train_loops.py
@@ -10,9 +10,12 @@ import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 from torch_geometric.nn import global_mean_pool
 
-from gnn.train_loops import graph_classification_train_loop
+from gnn.train_loops import graph_classification_train_loop, vae_training_loop
 from gnn.metrics import CrossEntropyAccuracyFromLogits
 from gnn.early_stopping import EarlyStopping
+
+from gnn.vae import VAE
+from gnn_data_generation.three_graph_families_dataset import ThreeGraphFamiliesDataset
 
 from utils import create_results_folder, get_baseline_folder
 
@@ -82,5 +85,49 @@ def test_class_train_loop(test_name, patience):
     
     # Evaluate model on one batch
     pred = model(next(iter(test_loader))).detach().numpy()
+    np.save(results_file, pred)
+    np.testing.assert_allclose(pred, np.load(baseline_file))
+
+@pytest.mark.parametrize("test_name, n_prior_modes, use_graph_enc, pos_weight",
+                         [
+                             ("vae_train", 0, False, None),
+                             ("vae_train_3modes", 0, False, None),
+                             ("vae_train_graphenc", 0, True, None),
+                             ("vae_train_posweight", 0, False, 1.),
+                         ]
+)
+def test_vae_train_loop(test_name, n_prior_modes, use_graph_enc, pos_weight):
+    """Test classification training loop"""
+    file_name = f'{test_name}.npy'
+    baseline_file = os.path.join(get_baseline_folder(test_name), file_name)
+    results_file = os.path.join(create_results_folder(test_name), file_name)
+
+    # Dataset
+    dataset = ThreeGraphFamiliesDataset(use_features=True)
+
+    torch.manual_seed(12345)
+    dataset = dataset.shuffle()
+
+    train_dataset = dataset[:150]
+    test_dataset = dataset[150:300]
+
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+    # Model definition
+    device = torch.device("cpu")
+    model = VAE(device=device, prior_modes=n_prior_modes, latent_dim=8, hidden_dim=16,
+                n_gin_layers=3, graph_enc=use_graph_enc, pos_weight=pos_weight)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.5e-4, weight_decay=0.5e-4)
+
+    # Training
+    _ = vae_training_loop(model=model, optimizer=optimizer, num_epochs=10, train_dl=train_loader,
+                          val_dl=test_loader, best_model_path=None, device=device,
+                          early_stopping=EarlyStopping(patience=10))
+    
+    # Evaluate model on one batch
+    model.eval()
+    _, _, pred, _ = model(next(iter(test_loader)))
+    pred = pred.detach().numpy()
     np.save(results_file, pred)
     np.testing.assert_allclose(pred, np.load(baseline_file))
