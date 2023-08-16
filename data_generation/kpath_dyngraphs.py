17fed4f3699a9628171d90640e22ba6babe82f45
a-mazzetto <92179628+a-mazzetto@users.noreply.github.com>
2023-08-16 12:22:38 +0200

    added support to extracting one graph

--- data_generation/kpath_dyngraphs.py
+++ data_generation/kpath_dyngraphs.py
@@ -14,7 +14,8 @@ def kpath_dyngraphs(
         interval = 60,
         n_intervals = 4,
         k_path_len = 3,
-        min_nodes = 10
+        min_nodes = 10,
+        target_node = None
 ):
     """Script to generate dynamic graphs from a dataset of type (time, source, destimation, anomaly) for training
     of a dynamic graph variational autoencoder
@@ -24,9 +25,18 @@ def kpath_dyngraphs(
     :param interval: aggregation time interval
     :param n_intervals: number of intervals in the dynamic graphs
     :param k_path_len: length of out-path links used to construct subgraphs
-    :param min_nodes: minimum number of nodes to consider the graph"""
+    :param min_nodes: minimum number of nodes to consider the graph
+    :param target_node: switches to extraction of dynamic graph for specific node"""
+    if target_node is not None and max_t != 1e8:
+        print("When target node is specified, max time input is disregarded")
     source_nodes = pd.read_table(scores_file, names=("score", "timewhen"), index_col=0,
                                  dtype={"score": float, "timewhen": float})
+    if target_node is not None:
+        target_time = source_nodes.loc[target_node].timewhen
+        min_t = target_time + (0.5 - n_intervals) * interval
+        max_t = target_time + 0.5 * interval
+    else:
+        min_t = 0
     # Create graphs
     macro_graphs = []
     current_links_list = []
@@ -39,26 +49,35 @@ def kpath_dyngraphs(
             time = float(time)
             if time > max_t:
                 break
-            macro_group = time // (interval * n_intervals)
-            if current_group is None:
-                current_group = macro_group
-            if current_t0 is None:
-                current_t0 = time
-            if macro_group != current_group:
-                graph = ig.Graph.TupleList(current_links_list, directed=True, weights=False)
-                graph.vs["score"] = source_nodes.loc[graph.vs["name"]].score.tolist()
-                graph.es["subgraph"] = subgraph_list
-                macro_graphs.append(graph)
-                current_group = macro_group
-                current_links_list = []
-                subgraph_list = []
-                current_t0 = time
-            if source in source_nodes.index:
-                subgraph_list.append(int((time - current_t0) // interval))
-                current_links_list.append((source, destination))
+            if time > min_t:
+                macro_group = (time - min_t) // (interval * n_intervals)
+                if current_group is None:
+                    current_group = macro_group
+                if current_t0 is None:
+                    current_t0 = time - min_t
+                if macro_group != current_group:
+                    graph = ig.Graph.TupleList(current_links_list, directed=True, weights=False)
+                    graph.vs["score"] = source_nodes.loc[graph.vs["name"]].score.tolist()
+                    graph.es["subgraph"] = subgraph_list
+                    macro_graphs.append(graph)
+                    current_group = macro_group
+                    current_links_list = []
+                    subgraph_list = []
+                    current_t0 = time - min_t
+                if source in source_nodes.index:
+                    subgraph_list.append(int((time - min_t - current_t0) // interval))
+                    current_links_list.append((source, destination))
+    # Discharge last graph
+    if len(current_links_list) > 0:
+        graph = ig.Graph.TupleList(current_links_list, directed=True, weights=False)
+        graph.vs["score"] = source_nodes.loc[graph.vs["name"]].score.tolist()
+        graph.es["subgraph"] = subgraph_list
+        macro_graphs.append(graph)
     # Extract graphs
     subgraphs = []
     for _node in source_nodes.index:
+        if target_node is not None and _node != target_node:
+            continue
         for _graph in macro_graphs:
             if _graph.vs.select(name=_node):
                 neighbors = _graph.neighborhood(vertices=_node, order=k_path_len, mode="out")
