ca45688c1f0b27d97f6b444db9573af93d6b08f4
a-mazzetto <92179628+a-mazzetto@users.noreply.github.com>
2023-08-22 14:22:06 +0200

    pvalue distribution script

---
+++ scripts/helper/lanl_pvalue_distribution.py
@@ -0,0 +1,24 @@
+"""Script to verify uniformity of p-value distribution in LANL dataset"""
+# %% Import
+import pandas as pd
+from scipy.stats import probplot
+import matplotlib.pyplot as plt
+
+# %% Load p-values
+py = pd.read_table(r"C:\Users\user\Desktop\phase_4_source_score_py.txt", names=("source", "score", "timewhen"))
+dp = pd.read_table(r"C:\Users\user\Desktop\phase_4_source_score_dp.txt", names=("source", "score", "timewhen"))
+ddcrp = pd.read_table(r"C:\Users\user\Desktop\phase_4_source_score_ddcrp.txt", names=("source", "score", "timewhen"))
+
+# %% Plot
+fig, ax = plt.subplots(1)
+probplot(py.score, dist="uniform", fit=False, plot=ax)
+probplot(dp.score, dist="uniform", fit=False, plot=ax)
+probplot(ddcrp.score, dist="uniform", fit=False, plot=ax)
+ax.plot([0, 1], [0, 1], "--", color="red")
+ax.get_lines()[0].set_color('blue')
+ax.get_lines()[1].set_color('cyan')
+ax.get_lines()[2].set_color('green')
+ax.get_lines()[3].set_color('red')
+ax.grid()
+
+# %%
